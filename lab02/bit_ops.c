16c2b7bd1f5be52a0b85519f799b1f48670e90ef
wplf <975761915@qq.com>
2023-10-24 19:41:22 +0800

    lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -5,23 +5,27 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
+    return (x & 1 << n) >> n;
     // YOUR CODE HERE
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
-    return -1;
+    // return -1;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
+                *x = *x & ~(1<<n)  | v << n;
     // YOUR CODE HERE
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
+               set_bit(x, n,  ~get_bit(*x, n) );
+                
     // YOUR CODE HERE
 }
 
